b81b2c1fd8e11366bf66cde37286127f81d090e3
= <=>
2020-10-20 17:37:16 -0400

    Add initial files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');	
+
+gulp.task('sass', function() {
+//root scss file (import all your partials into here)
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        // add vendor prefixes
+        .pipe(autoprefixer())
+        // change the file name to be styles.scss.liquid file
+        .pipe(rename('styles.css.liquid'))
+        // remove the extra set of quotations used for escaping the liquid string (we'll explain this in a sec)
+        .pipe(replace('"{{', '{{'))
+        .pipe(replace('}}"', '}}'))
+        // save the file to the theme assets directory
+        .pipe(gulp.dest('./assets/'));
+});
+
+
+gulp.task('default', function() {
+    // this assumes your sass is in a directory named sass
+    gulp.watch(['./sass/**/*.scss'], gulp.series('sass'));
+});
+
